8f4d4ec3e1539f7695a0827c7af0954c3289ea1d
Mohamed Esmail <153458225+MoEsmail-22@users.noreply.github.com>
2025-04-15 01:19:51 +0200

    Update signin.js

--- js/signin.js
+++ js/signin.js
@@ -1,3 +1,10 @@
 'use strict';
-const email = document.getElementById(email).value;
-const password = document.getElementById(password).value;
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const singIn = document.querySelector('.forgot');
+
+singIn.addEventListener('click', function () {
+  if (!email && !password) {
+    alert('Enter ur email and pssword');
+  } else location.href = 'index.html';
+});
