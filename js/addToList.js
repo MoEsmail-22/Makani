91431540ce0ce6a8eef6d48b2e8b81e5bbba16b7
Mohamed Esmail <153458225+MoEsmail-22@users.noreply.github.com>
2025-04-15 21:58:50 +0200

    Add files via upload

---
+++ js/addToList.js
@@ -0,0 +1,50 @@
+'use stirct';
+
+function openNav() {
+  document.getElementById('mySidenav').style.width = '250px';
+}
+
+function closeNav() {
+  document.getElementById('mySidenav').style.width = '0';
+}
+
+////////////////add the 360 img
+
+const add360But = document.querySelector('.overlay360');
+const modle = document.querySelector('.overlay');
+const close = document.querySelectorAll('.close-modal');
+const ok = document.querySelector('.ok');
+const checkButtons = document.querySelectorAll('.button360');
+const form = document.querySelector('.form');
+const done = document.querySelector('.done');
+
+let fees = 0;
+
+add360But.addEventListener('click', function (e) {
+  e.preventDefault();
+  modle.classList.remove('none');
+  document.querySelector('.blure').style.filter = 'blur(50px)';
+});
+
+close.forEach(btn => {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    modle.classList.add('none');
+    document.querySelector('.blure').style.filter = 'none';
+  });
+});
+
+ok.addEventListener('click', function (e) {
+  e.preventDefault();
+  checkButtons.forEach(button => {
+    button.classList.add('none');
+    form.classList.remove('none');
+    done.classList.remove('none');
+    fees += 100;
+  });
+});
+
+done.addEventListener('click', function (e) {
+  e.preventDefault();
+  modle.classList.add('none');
+});
